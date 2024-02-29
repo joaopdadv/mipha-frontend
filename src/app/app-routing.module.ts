7444fdee753a2ca6f9baefaa64dbdb30d4c7ed3e
joaopdadv <joaopdadv@gmail.com>
2024-02-29 11:18:31 -0300

    estrutura inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginModalComponent } from './pages/login/login-modal/login-modal.component';
+import { RegisterModalComponent } from './pages/login/register-modal/register-modal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", pathMatch:"full", redirectTo: "login"},
+  {path: 'login', component: LoginComponent, children: [
+    {path: "", component: LoginModalComponent},
+    {path: "register", component: RegisterModalComponent}
+
+  ]},
+  {path: "home", component: HomeComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginModalComponent } from './pages/login/login-modal/login-modal.component';
+import { RegisterModalComponent } from './pages/login/register-modal/register-modal.component';
+import { ModalComponent } from './components/modal/modal.component';
+import { ButtonComponent } from './components/button/button.component';
+import { InputComponent } from './components/input/input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    LoginModalComponent,
+    RegisterModalComponent,
+    ModalComponent,
+    ButtonComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
